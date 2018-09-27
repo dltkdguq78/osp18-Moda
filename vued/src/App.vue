c1fa4a6f6c90b9daeecdc293cd6e4293100c025e
20140915 <dltkdguq78@naver.com>
2018-09-27 21:17:36 +0900

    체크 완성 및 에딧버튼 완성 (최종)

--- vued/src/App.vue
+++ vued/src/App.vue
@@ -4,7 +4,7 @@
     <TodoInput v-on:addTodo="addTodo"></TodoInput>
     <!-- TodoList 의 Template 를 추가할 때 동작할 propdata 를 bind 한다.
      이후 completeTodo 메소드와 removeTodo 메소드에서 동작할 상위 함수를 bind한다. -->
-    <TodoList v-bind:propsdata="todoItems" @completeTodo="completeTodo" @removeTodo="removeTodo"></TodoList>
+    <TodoList v-bind:propsdata="todoItems" @completeTodo="completeTodo" @removeTodo="removeTodo" @editTodo="editTodo"></TodoList>
     <TodoFooter v-on:removeAll="clearAll"></TodoFooter>
   </div>
 </template>
@@ -26,36 +26,35 @@ export default {
       localStorage.clear();
       this.todoItems = [];
     },
-		addTodo(todoItem) {
+      addTodo(todoItem) {
       localStorage.setItem(todoItem, JSON.stringify({'context':todoItem, 'isComplete':false}));
-			this.todoItems.push({'context':todoItem, 'isComplete':false});
-		},
+         this.todoItems.push({'context':todoItem, 'isComplete':false});
+      },
     removeTodo(todoItem, index) {
       localStorage.removeItem(todoItem);
       this.todoItems.splice(index, 1);
     },
     // Todo 상태를 변경시키기 위한 함수
     completeTodo(todoItem, index){
-      localStorage.setItem(todoItem,JSON.stringify({'context':todoItem, 'isComplete':true}));//로컬스토리지에 들어갈때 string으로 변환되기 때문에 json를 사용하여 객체를 주고 받는다.
-      this.todoItems[index]['isComplete'] = true; //todoItems에 들어있는 Data의 Index를 조회하고 isComplete 키 값의 Value 데이터를 true로 변경시킨다.
-    }
+        todoItem.isComplete = !todoItem.isComplete;
+        localStorage.setItem(todoItem.context,JSON.stringify({'context':todoItem.context, 'isComplete':todoItem.isComplete}));
+    },
+   editTodo(todoItem,editTodoItem,index){
+        localStorage.setItem(editTodoItem,JSON.stringify({'context':editTodoItem, 'isComplete':todoItem.isComplete}));
+      localStorage.removeItem(todoItem.context);
+      this.todoItems[index].context = editTodoItem;
+        }
   },
   created() {
-		if (localStorage.length > 0) {
-			for (var i = 0; i < localStorage.length; i++) {
-        // Dict (딕셔너리) Key:Value
-        // context : localStorage.Key(i) => 할 일 문자열
-        // isComplete : false => Boolean
+      if (localStorage.length > 0) {
+         for (var i = 0; i < localStorage.length; i++) {
         this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-				//this.todoItems.push({'context':localStorage.key(i), 'isComplete':true});
       }
-		}
+   }
   },
   components: {
-    'TodoHeader': TodoHeader,
-    'TodoInput': TodoInput,
-    'TodoList': TodoList,
-    'TodoFooter': TodoFooter
+    'TodoHeader': TodoHeader,    'TodoInput': TodoInput,
+    'TodoList': TodoList,    'TodoFooter': TodoFooter
   }
 }
 </script>
